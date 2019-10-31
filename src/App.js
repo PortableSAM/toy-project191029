a054c1125fe0f680cccb4a634869b75026111b27
Lee-h-saem <53569830+Lee-h-saem@users.noreply.github.com>
2019-11-01 00:14:51 +0900

    create layout, menu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import React, { Component } from "react";
+import { Layout, Header, Navigation, Drawer, Content } from "react-mdl";
 import "./App.css";
+import Main from "./components/main";
+import { Link } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>hello React</h1>
+      <div className="demo-big-content">
+        <Layout>
+          <Header title="Title" scroll>
+            <Navigation>
+              <Link to="/resume">Resume</Link>
+              <Link to="/about">About</Link>
+              <Link to="/project">Project</Link>
+              <Link to="/contact">Contact</Link>
+            </Navigation>
+          </Header>
+          <Drawer title="Title">
+            <Navigation>
+              <Link to="/resume">Resume</Link>
+              <Link to="/about">About</Link>
+              <Link to="/project">Project</Link>
+              <Link to="/contact">Contact</Link>
+            </Navigation>
+          </Drawer>
+          <Content>
+            <div className="page-content" />
+            <Main />
+          </Content>
+        </Layout>
       </div>
     );
   }
