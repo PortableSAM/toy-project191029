db8b5f4ec2e586eef3053f93aff72b2274df530d
Lee-h-saem <53569830+Lee-h-saem@users.noreply.github.com>
2019-11-01 23:32:35 +0900

    Designing Project Page

--- src/components/project.js
+++ src/components/project.js
@@ -1,5 +1,17 @@
 import React, { Component } from "react";
-import { Tabs, Tab } from "react-mdl";
+import {
+  Tabs,
+  Tab,
+  Grid,
+  Cell,
+  Card,
+  CardTitle,
+  CardActions,
+  CardText,
+  Button,
+  CardMenu,
+  IconButton
+} from "react-mdl";
 
 class Project extends Component {
   constructor(props) {
@@ -10,13 +22,55 @@ class Project extends Component {
     if (this.state.activeTab === 0) {
       return (
         <div>
-          <h1>This is React</h1>
+          {/* Project 1 */}
+          <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+            <CardTitle
+              style={{
+                color: "#fff",
+                height: "176px",
+                background:
+                  "url(https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png) center / cover"
+              }}
+            >
+              React Project #1
+            </CardTitle>
+            <CardText>lalalalal</CardText>
+            <CardActions border>
+              <Button colored>Github</Button>
+              <Button colored>CodePen</Button>
+              <Button colored>Live Demo</Button>
+            </CardActions>
+            <CardMenu style={{ color: "#fff" }}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
         </div>
       );
     } else if (this.state.activeTab === 1) {
       return (
         <div>
-          <h1>This is JavaScript</h1>
+          {/* Project 2 */}
+          <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+            <CardTitle
+              style={{
+                color: "#fff",
+                height: "176px",
+                background:
+                  "url(https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fk.kakaocdn.net%2Fdn%2FdDU2wy%2Fbtqv3I89S7A%2F5OuBt7iL7aTVFjopV0XbV0%2Fimg.png) center / cover"
+              }}
+            >
+              JavaScript Project #1
+            </CardTitle>
+            <CardText>lalalalal</CardText>
+            <CardActions border>
+              <Button colored>Github</Button>
+              <Button colored>CodePen</Button>
+              <Button colored>Live Demo</Button>
+            </CardActions>
+            <CardMenu style={{ color: "#fff" }}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
         </div>
       );
     }
@@ -34,7 +88,11 @@ class Project extends Component {
           <Tab>JavaScript</Tab>
         </Tabs>
 
-        <section className="projects-grid">{this.toggleCategoris()}</section>
+        <Grid>
+          <Cell col={12}>
+            <div className="content">{this.toggleCategoris()}</div>
+          </Cell>
+        </Grid>
       </div>
     );
   }
