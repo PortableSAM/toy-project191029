be6fe10e0c404a80d192e926481eb94e65136eed
Lee-h-saem <53569830+Lee-h-saem@users.noreply.github.com>
2019-11-01 23:08:39 +0900

    Written modification

--- src/components/project.js
+++ src/components/project.js
@@ -1,10 +1,40 @@
 import React, { Component } from "react";
+import { Tabs, Tab } from "react-mdl";
 
 class Project extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { activeTab: 0 };
+  }
+  toggleCategoris() {
+    if (this.state.activeTab === 0) {
+      return (
+        <div>
+          <h1>This is React</h1>
+        </div>
+      );
+    } else if (this.state.activeTab === 1) {
+      return (
+        <div>
+          <h1>This is JavaScript</h1>
+        </div>
+      );
+    }
+  }
+
   render() {
     return (
-      <div>
-        <h1>Project Page</h1>
+      <div className="category-tabs">
+        <Tabs
+          activeTab={this.state.activeTab}
+          onChange={tabId => this.setState({ activeTab: tabId })}
+          ripple
+        >
+          <Tab>React</Tab>
+          <Tab>JavaScript</Tab>
+        </Tabs>
+
+        <section className="projects-grid">{this.toggleCategoris()}</section>
       </div>
     );
   }
